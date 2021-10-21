4a8071ac1b6e2a1dc452eb532dda647b1a5244fd
konradsiekacz <konradsiekacz@gmail.com>
2021-10-21 21:10:59 +0200

    Initial commit

---
+++ src/test/java/com/konrad/genderdetector/GenderDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konrad.genderdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
