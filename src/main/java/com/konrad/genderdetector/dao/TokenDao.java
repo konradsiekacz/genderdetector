206d9119072b529e1f9a4be3f433c4cb7c5ddd23
konradsiekacz <konradsiekacz@gmail.com>
2021-10-24 12:17:34 +0200

    add method to read tokens from file and method to return all names for specific gender

--- src/main/java/com/konrad/genderdetector/dao/TokenDao.java
+++ src/main/java/com/konrad/genderdetector/dao/TokenDao.java
@@ -1,9 +1,7 @@
 package com.konrad.genderdetector.dao;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public interface TokenDao {
-    String getFemaleNames(String fileName) throws IOException;
-    String getMaleNames(String fileName);
+    String getFemaleNames();
+    String getMaleNames();
 }
--- src/main/java/com/konrad/genderdetector/dao/TokenNameDao.java
+++ src/main/java/com/konrad/genderdetector/dao/TokenNameDao.java
@@ -1,35 +1,40 @@
 package com.konrad.genderdetector.dao;
 
+import org.springframework.stereotype.Component;
+
 import java.io.*;
-import java.util.Scanner;
+import java.util.stream.Collectors;
 
+@Component
 public class TokenNameDao implements TokenDao {
-    private final String FEMALE_NAME = "/femaleNames.txt";
-    private final String MALE_NAME = "/maleNames.txt";
+    private final String FEMALE_NAME = "femaleNames.txt";
+    private final String MALE_NAME = "maleNames.txt";
 
     @Override
-    public String getFemaleNames(String fileName) throws IOException {
+    public String getFemaleNames() {
+        return readFromInputStream(FEMALE_NAME);
+    }
+
+    @Override
+    public String getMaleNames() {
+        return readFromInputStream(MALE_NAME);
+    }
+
+
+    public String readFromInputStream(String fileName) {
+        String names = "";
         ClassLoader classLoader = getClass().getClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream(FEMALE_NAME);
-        String data = readFromInputStream(inputStream);
-        return data;
-    }
-
-    @Override
-    public String getMaleNames(String fileName) {
-        return null;
-    }
-
-    private String readFromInputStream(InputStream inputStream)
-            throws IOException {
-        StringBuilder resultStringBuilder = new StringBuilder();
-        try (BufferedReader br
-                     = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                resultStringBuilder.append(line).append("\n");
-            }
+        File file = new File(classLoader.getResource(fileName).getFile());
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader br = new BufferedReader(fileReader);
+            names = br.lines().collect(Collectors.joining("\n"));
+            br.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Files does not exist");
+        } catch (IOException e) {
+            System.err.println("File exists, but there was IOException");
         }
-        return resultStringBuilder.toString();
+        return names;
     }
 }
