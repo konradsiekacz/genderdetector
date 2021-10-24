c5014e8ba1e481982ac97f8901a5232b25f5b411
konradsiekacz <konradsiekacz@gmail.com>
2021-10-24 13:51:55 +0200

    add response status to getAllNames method

--- src/main/java/com/konrad/genderdetector/controller/GenderController.java
+++ src/main/java/com/konrad/genderdetector/controller/GenderController.java
@@ -2,10 +2,11 @@ package com.konrad.genderdetector.controller;
 
 import com.konrad.genderdetector.service.GenderService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -14,8 +15,9 @@ public class GenderController {
 
     private final GenderService genderService;
 
-    @GetMapping("/females")
-    public List<?> getAllFemaleNames() {
+    @GetMapping("/names")
+    @ResponseStatus(HttpStatus.OK)
+    public List<?> getAllNames() {
         return genderService.getAllFemaleNames();
     }
 
