5d8b1f05b293ddeee8099bb90ceae7ccb3951048
konradsiekacz <konradsiekacz@gmail.com>
2021-10-31 18:29:41 +0100

    create new method for scan through file and for return all names from file

--- src/main/java/com/konrad/genderdetector/dao/TokenNameDao.java
+++ src/main/java/com/konrad/genderdetector/dao/TokenNameDao.java
@@ -1,39 +1,80 @@
 package com.konrad.genderdetector.dao;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.springframework.stereotype.Repository;
 
 import java.io.*;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 @Repository
 public class TokenNameDao implements TokenDao {
-    private final String FEMALE_NAME = "femaleNames.txt";
-    private final String MALE_NAME = "maleNames.txt";
+    private final String FEMALE_NAME = "/femaleNames.txt";
+    private final String MALE_NAME = "/maleNames.txt";
+    private InputStream inputStream = null;
+    private Scanner scanner = null;
 
     @Override
     public String getFemaleNames() {
-        return readFromInputStream(FEMALE_NAME);
+        return getAllNamesFromFile(FEMALE_NAME);
     }
 
     @Override
     public String getMaleNames() {
-        return readFromInputStream(MALE_NAME);
+        return getAllNamesFromFile(MALE_NAME);
     }
 
-    public String readFromInputStream(String fileName) {
-        String names = "";
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(fileName).getFile());
-        try {
-            FileReader fileReader = new FileReader(file);
-            BufferedReader br = new BufferedReader(fileReader);
-            names = br.lines().collect(Collectors.joining("\n"));
-            br.close();
-        } catch (FileNotFoundException e) {
-            System.err.println("Files does not exist");
-        } catch (IOException e) {
-            System.err.println("File exists, but there was IOException");
-        }
-        return names;
+    private Scanner scanThroughFile(String pathFile) {
+        inputStream = getClass().getResourceAsStream(pathFile);
+        scanner = new Scanner(inputStream, "UTF-8");
+        return scanner;
     }
+
+    private String getAllNamesFromFile(String pathFile) {
+        StringBuilder allNames = new StringBuilder();
+        scanner = scanThroughFile(pathFile);
+        while (scanner.hasNextLine()) {
+            allNames.append(scanner.nextLine()).append("\n");
+        }
+        return allNames.toString();
+    }
+
+
+//    public String readFromInputStream(String fileName) {
+//        String names = "";
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        File file = new File(classLoader.getResource(fileName).getFile());
+//        try {
+//            FileReader fileReader = new FileReader(file);
+//            BufferedReader br = new BufferedReader(fileReader);
+//            names = br.lines().collect(Collectors.joining("\n"));
+//            br.close();
+//        } catch (FileNotFoundException e) {
+//            System.err.println("Files does not exist");
+//        } catch (IOException e) {
+//            System.err.println("File exists, but there was IOException");
+//        }
+//        return names;
+
+//    }
+
+    //Use apache commons I/O
+//    private LineIterator readThroughFile(File fileName, String encoderType) throws IOException {
+//        LineIterator it = FileUtils.lineIterator(fileName, "UTF-8");
+//        return it;
+//    }
+
+    //Use apache commons I/O
+//    private String getAllNamesFromFile2(String pathFile) throws IOException {
+//        StringBuilder allNames = new StringBuilder();
+//        LineIterator it = readThroughFile(new File(pathFile), "UTF-8");
+//
+//        while (it.hasNext()) {
+//            allNames.append(it.nextLine()).append("\n");
+//        }
+//        return allNames.toString();
+//    }
+
+
 }
