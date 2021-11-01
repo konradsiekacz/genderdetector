3f8e6abb5c5d66252e60e8ce7b08bdd81ba02137
konradsiekacz <konradsiekacz@gmail.com>
2021-11-01 20:54:01 +0100

    create class for testing service

--- src/test/java/com/konrad/genderdetector/controller/TestConfig.java
+++ src/test/java/com/konrad/genderdetector/controller/TestConfig.java
@@ -2,7 +2,6 @@ package com.konrad.genderdetector.controller;
 
 import com.konrad.genderdetector.service.GenderService;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
---
+++ src/test/java/com/konrad/genderdetector/service/GenderServiceTest.java
@@ -0,0 +1,4 @@
+package com.konrad.genderdetector.service;
+
+public class GenderServiceTest {
+}
